4b646a78e441aba3599f1780137062d57a541349
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-01-09 17:05:50 -0600

    testing commit with VS code

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,37 @@
+var quizQuestion = [
+  {
+    question: "Which is NOT a data type in JavaScript?",
+    answers: [
+      { choice: "string", correct: false },
+      { choice: "number", correct: false },
+      { choice: "attribute", correct: true },
+      { choice: "boolean", correct: false },
+    ],
+  },
+  {
+    question: "What HTML tag is used for Javascript?",
+    answers: [
+      { choice: "<script>", correct: true },
+      { choice: "<js>", correct: false },
+      { choice: "<java>", correct: false },
+      { choice: "<javascript>", correct: false },
+    ],
+  },
+  {
+    question: "Which symbol is a strict comparison?",
+    answers: [
+      { choice: " === ", correct: true },
+      { choice: " = ", correct: false },
+      { choice: " == ", correct: false },
+      { choice: " != ", correct: false },
+    ],
+  },
+];
+
 //variables from the DOM
 
 var timeElement = document.getElementById("timer");
+var displayQuestion = document.getElementById("quiz-question");
 
 //varibales in global scope
 var countdown = 100;
@@ -15,5 +46,6 @@ function startTimer() {
     }
   }, 1000);
 }
-
 startTimer();
+
+displayQuestion.textContent = quizQuestion.question[0];
