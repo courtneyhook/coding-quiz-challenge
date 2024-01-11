08198509cb70a8730b4ddc82d8ce8829494c6e87
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-01-11 16:34:12 -0600

    added answer choice buttons

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # coding-quiz-challenge
+
 This challenge tests the user's knowledge about JavaScript trivia and produces an end score.
+https://basescripts.com/110-javascript-quiz-questions-with-solutions-pdf-download-test-your-knowledge
--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,66 @@
 var quizQuestion = [
-  {
-    question: "Which is NOT a data type in JavaScript?",
-    answers: [
-      { choice: "string", correct: false },
-      { choice: "number", correct: false },
-      { choice: "attribute", correct: true },
-      { choice: "boolean", correct: false },
-    ],
-  },
-  {
-    question: "What HTML tag is used for Javascript?",
-    answers: [
-      { choice: "<script>", correct: true },
-      { choice: "<js>", correct: false },
-      { choice: "<java>", correct: false },
-      { choice: "<javascript>", correct: false },
-    ],
-  },
-  {
-    question: "Which symbol is a strict comparison?",
-    answers: [
-      { choice: " === ", correct: true },
-      { choice: " = ", correct: false },
-      { choice: " == ", correct: false },
-      { choice: " != ", correct: false },
-    ],
-  },
+  [
+    "What is not a datatype in JavaScript?",
+    "string",
+    "boolean",
+    "variable",
+    "number",
+  ],
+  [
+    "Which of the following is not a comparison operator in JavaScript?",
+    "==",
+    "===",
+    "!=",
+    "=>",
+  ],
+  [
+    "What is the output of the following code: \nvar x = 5;\nconsole.log(x--);",
+    "x--",
+    "5",
+    "4",
+    "6",
+  ],
+  ["Q4", "A1", "A2", "A3", "A4"],
+  ["Q5", "A1", "A2", "A3", "A4"],
+  ["Q6", "A1", "A2", "A3", "A4"],
+  ["Q7", "A1", "A2", "A3", "A4"],
+  ["Q8", "A1", "A2", "A3", "A4"],
+  ["Q9", "A1", "A2", "A3", "A4"],
+  ["Q10", "A1", "A2", "A3", "A4"],
+  ["Q11", "A1", "A2", "A3", "A4"],
+  ["Q12", "A1", "A2", "A3", "A4"],
+  ["Q13", "A1", "A2", "A3", "A4"],
+  ["Q14", "A1", "A2", "A3", "A4"],
 ];
 
+var quizAnswer = [
+  "variable",
+  "=>",
+  "4",
+  "Q4A",
+  "Q5A",
+  "Q6A",
+  "Q7A",
+  "Q8A",
+  "Q9A",
+  "Q10A",
+  "Q11A",
+  "Q12A",
+  "Q13A",
+  "Q14A",
+];
 //variables from the DOM
 
 var timeElement = document.getElementById("timer");
 var displayQuestion = document.getElementById("quiz-question");
+var chooseA = document.getElementById("choice-a");
+var chooseB = document.getElementById("choice-b");
+var chooseC = document.getElementById("choice-c");
+var chooseD = document.getElementById("choice-d");
 
-//varibales in global scope
+//variables in global scope
 var countdown = 100;
+var currentQuestion;
 
 function startTimer() {
   var countTimer = setInterval(function () {
@@ -46,6 +72,15 @@ function startTimer() {
     }
   }, 1000);
 }
+
+function showQuestion(i) {
+  displayQuestion = "";
+  displayQuestion = quizQuestion[i][0];
+}
 startTimer();
 
-displayQuestion.textContent = quizQuestion.question[0];
+displayQuestion.textContent = quizQuestion[2][0];
+chooseA.textContent = quizQuestion[2][1];
+chooseB.textContent = quizQuestion[2][2];
+chooseC.textContent = quizQuestion[2][3];
+chooseD.textContent = quizQuestion[2][4];
